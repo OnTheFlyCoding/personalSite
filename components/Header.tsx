8dc9c75fad86f31ea222d47398d14f3fd09dcb29
Giovanni Gonzalez <createdtyler@gmail.com>
2025-02-22 19:55:56 -0500

    base project

---
+++ components/Header.tsx
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import React from 'react'
+// Components
+import NavBar from './NavBar'
+import { Button } from './ui/button'
+
+const Header = () => {
+  return (
+    <header className='py-8 lg:py-12 text-white bg-pink-50/20'>
+        <div className='container mx-auto flex justify-between items-center '>
+            <Link href='/'>
+            <h1 className='font-semibold text-4xl'>
+                Gio <span className='text-accent'>.</span>
+            </h1>
+            </Link>
+             {/* Desktop nav & Hire ME*/}
+             <div className='hidden xl:flex items-center gap-8 '>
+                <NavBar/>
+                <Link href='/contact'>
+                <Button> Hire Me</Button>
+                </Link>
+             </div>
+             {/* Mobile Nav */}
+             <div className="xl:hidden">
+                 Mobile Nav
+             </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
